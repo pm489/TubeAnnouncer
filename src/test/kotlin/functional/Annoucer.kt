22976d4058fa62a8949475f1b23a40c6992ad1da
Phillip Meredith <shada@Phillips-MacBook-Pro-2.local>
2018-07-17 16:56:17 +0100

    Set up an announcer who can call announce

---
+++ src/test/kotlin/functional/Annoucer.kt
@@ -0,0 +1,38 @@
+package functional
+
+import com.natpryce.hamkrest.assertion.assertThat
+import com.natpryce.hamkrest.equalTo
+
+class AnnouncerTest{
+
+    @org.junit.Test
+    fun `can call announce at correct time`() {
+        val textToVoice  = FakeTextToVoice()
+
+        Announcer(textToVoice, TubeLine("what-ever"), TubeStop("some-stop")).go()
+
+        assertThat(textToVoice.called, equalTo(1))
+    }
+}
+
+class Announcer(val textToVoice: TextToVoice, val line: TubeLine, val stop: TubeStop) {
+    fun go() {
+        textToVoice.announce("Next train has arrived")
+    }
+
+}
+
+data class TubeStop(val value: String)
+
+data class TubeLine(val value: String)
+
+interface TextToVoice {
+    fun announce(text: String)
+}
+
+class FakeTextToVoice : TextToVoice {
+    var called = 0
+    override fun announce(text: String) {
+        called++
+    }
+}
